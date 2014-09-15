f5d148cdc426e7209ee2bbd44fa4a9b9df54a81c
Ben Connito <ben.connito@exponentialent.com>
2014-09-15 17:01:24 -0400

    just strip the '.' from uniqid with added entropy. this way we dont run into any md5 collisions.

--- src/Ratchet/Wamp/WampConnection.php
+++ src/Ratchet/Wamp/WampConnection.php
@@ -17,7 +17,7 @@ class WampConnection extends AbstractConnectionDecorator {
         parent::__construct($conn);
 
         $this->WAMP            = new \StdClass;
-        $this->WAMP->sessionId = md5(uniqid(mt_rand(), true));
+        $this->WAMP->sessionId = str_replace('.','',uniqid(mt_rand(), true));
         $this->WAMP->prefixes  = array();
 
         $this->send(json_encode(array(WAMP::MSG_WELCOME, $this->WAMP->sessionId, 1, \Ratchet\VERSION)));
