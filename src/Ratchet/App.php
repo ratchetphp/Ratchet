c167b476b817500f2a6fd32291629b88e815eea6
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2014-12-27 11:09:41 +0100

    Typo
    endpiont -> endpoint

--- src/Ratchet/App.php
+++ src/Ratchet/App.php
@@ -95,7 +95,7 @@ class App {
     }
 
     /**
-     * Add an endpiont/application to the server
+     * Add an endpoint/application to the server
      * @param string             $path The URI the client will connect to
      * @param ComponentInterface $controller Your application to server for the route. If not specified, assumed to be for a WebSocket
      * @param array              $allowedOrigins An array of hosts allowed to connect (same host by default), ['*'] for any
