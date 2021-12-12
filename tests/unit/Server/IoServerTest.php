6512da0c6c133a1e8992e65fae9642b7a535ab04
Chris Boden <cboden@gmail.com>
2021-12-12 11:35:34 -0500

    Lock minimum implicity dependency

--- tests/unit/Server/IoServerTest.php
+++ tests/unit/Server/IoServerTest.php
@@ -18,7 +18,7 @@ class IoServerTest extends \PHPUnit_Framework_TestCase {
     protected $reactor;
 
     protected function tickLoop(LoopInterface $loop) {
-        $loop->addTimer(0, function() use ($loop) {
+        $loop->futureTIck(function () use ($loop) {
             $loop->stop();
         });
 
